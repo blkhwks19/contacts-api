8d230a2539352ed764a298e363351e922030881f
Jeff Schaefer <blkhwks19@yahoo.com>
2020-10-09 16:11:00 -0500

    Added hack to allow client to make request from localhost while api is running on localhost as well

--- index.js
+++ index.js
@@ -5,7 +5,11 @@ const ContactModel = require('./models/Contact');
 const port = 3002;
 const app = express();
 
-
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true,
